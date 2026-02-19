1a037b2bed96d8144c1907ee44cb7a93f61ef649
idheeraj2603 <dheerajy2603@gmail.com>
2026-02-19 13:51:54 +0530

    refactor main.cpp for LDR sensor integration and add migration configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,28 @@
-#include <Arduino.h>
 /**
- * @file main.ino
+ * @file main.cpp
  * @brief Embedded Light Intensity Monitoring using LDR
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Dheeraj Yadav [idheeraj2603]
+ * @date 19-02-2026
  *
  * @details
  * Reads analog brightness data from LDR sensor
  * and displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define LDR analog pin (Use A0)
-
- // TODO 2:
- // Create variable to store sensor reading
-
+#include <Arduino.h>
+int ldrPin = A0;
 void setup() {
+    Serial.begin(9600);
+    while (!Serial);
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
 
-    // TODO 4:
-    // Print system initialization message
+    // write your initialization code here
 }
-
 void loop() {
-
-    // TODO 5:
-    // Read analog value from LDR
-
-    // TODO 6:
-    // Print raw ADC value
-
-    // TODO 7:
-    // Apply threshold logic (Bright / Dark detection)
-
-    // TODO 8:
-    // Print brightness status
-
-    // TODO 9:
-    // Add delay (500ms or 1 second)
+    int ldrValue = analogRead(ldrPin);
+    int lightPercent=map(ldrValue, 0, 1023, 0, 100);
+    Serial.print("LDR Value: ");
+    Serial.println(ldrValue);
+    delay(300);
 }
+// write your code here
